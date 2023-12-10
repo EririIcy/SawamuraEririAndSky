15be1f08347a8cfa92b11f241e5b53efccc1e9f7
Yaoyao <37120222203287@stu.xmu.edu.cn>
2023-12-10 18:41:24 +0800

    添加了DMP相关的库，修改了README的格式，更改中文编码为GBK

---
+++ cmake-build-debug/CMakeFiles/SawamuraEririAndSky.elf.dir/Core/Src/lcd.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/SawamuraEririAndSky.elf.dir/Core/Src/lcd.c.obj: \
+ D:\PersonalFile\STM32F429\FinalWork\git_SawamuraEririAndSky\Core\Src\lcd.c \
+ D:\PersonalFile\STM32F429\FinalWork\git_SawamuraEririAndSky\Core\Inc/lcd.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\gnu_tools_arm_embedded\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\PersonalFile\STM32F429\FinalWork\git_SawamuraEririAndSky\Core\Inc/lcd_init.h \
+ D:\PersonalFile\STM32F429\FinalWork\git_SawamuraEririAndSky\Core\Inc/lcdfont.h
